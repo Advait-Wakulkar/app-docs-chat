1510e9072a540d0c25765d9a85fb8cf196cddfea
Advait-Wakulkar <wakulkar.a@northeastern.edu>
2025-02-24 21:13:34 -0500

    add Clerk authentication: implement sign-in and sign-up pages, middleware, and integrate ClerkProvider in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,33 @@
 import "@/styles/globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 import { TRPCReactProvider } from "@/trpc/react";
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <header className="flex justify-end items-center p-4 gap-4 h-16">
+          <SignedOut>
+            <SignInButton />
+            <SignUpButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </header>
+        {children}
       </body>
     </html>
+  </ClerkProvider>
   );
 }
