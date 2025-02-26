b509fecadfa5078962a618c673129a28c184bb82
Advait-Wakulkar <wakulkar.a@northeastern.edu>
2025-02-26 00:43:50 -0500

    remove unused hello API call from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import { api } from "@/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
   void api.post.getLatest.prefetch();
 
   return (
