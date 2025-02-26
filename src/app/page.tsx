291b392f76f3516db9ea7bdd3ddba70360599caa
Advait-Wakulkar <wakulkar.a@northeastern.edu>
2025-02-26 00:48:11 -0500

    add hello API call to Home component and update SyncUser button functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
+"use-client"
+
 import { api } from "@/trpc/server";
 import SyncUser from "./sync-user/page";
 
 export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+
   void api.post.getLatest.prefetch();
 
   return (
    <div>
     <h1>Hello World</h1>
-    <button onClick={SyncUser}>Sync-User</button>
    </div>
   );
 }
